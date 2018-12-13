dc0c06eec50043a9047405792b768d5eec255767
davreimz <fR33m4nn>
2018-12-13 20:57:40 +0200

    available support

--- src/main/java/com/reach_u/kov48/model/Support.java
+++ src/main/java/com/reach_u/kov48/model/Support.java
@@ -1,11 +1,20 @@
 package com.reach_u.kov48.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Support {
 
     private String type;
 
     private int amount;
 
-    private boolean applied;
+    private String status;
+
+    private boolean selected;
 
 }
--- src/main/java/com/reach_u/kov48/rest/controller/SupportServiceImpl.java
+++ src/main/java/com/reach_u/kov48/rest/controller/SupportServiceImpl.java
@@ -1,5 +1,6 @@
 package com.reach_u.kov48.rest.controller;
 
+import com.google.common.collect.Lists;
 import com.reach_u.kov48.model.Child;
 import com.reach_u.kov48.model.Support;
 import com.reach_u.kov48.service.SupportService;
@@ -10,9 +11,23 @@ import java.util.Collection;
 @Service
 public class SupportServiceImpl implements SupportService {
 
+    private static final String PAID = "Makstud";
+    private static final String NOT_APPLIED = "Esitamata";
+    private static final String PROCESSING = "Menetluses";
+
+
+    private static final Collection<Support> SUPPORT = Lists.newArrayList();
+
+    static {
+        SUPPORT.add(new Support("Sünnitoetus (Riik)", 320, PAID, true));
+        SUPPORT.add(new Support("Sünnitoetus (KOV)", 1000, PAID, true));
+        SUPPORT.add(new Support("Lepsetoetus (Riik)", 32, NOT_APPLIED, false));
+        SUPPORT.add(new Support("Vanemahüvitis (Riik)", 1500, NOT_APPLIED, false));
+    }
+
     @Override
     public Collection<Support> availableSupport(Child child) {
-        return null;
+        return SUPPORT;
     }
 
     @Override
